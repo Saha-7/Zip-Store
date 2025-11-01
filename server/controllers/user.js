04d29789e46410b7cbd18364983eeb6e6c304f12
saha-7op <saha7pritam@gmail.com>
2025-11-01 20:38:58 +0530

    hashPassword + save to DB

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,25 +1,44 @@
 import UserModel from "../models/user";
+import { validateSignUpData } from "../utils/validation";
+import bcrypt from 'bcrypt';
+
+const saltRounds = 13
 
 export async function registerUserController(request, response) {
   try {
+
+    validateSignUpData(request)
+
     const { name, email, password } = request.body;
-    if(!name || !email || !password){
-        return response.status(400).json({
-            message: "Provide all the fields",
-            error: true,
-            success: false
-        })
-    }
-    const user = await UserModel.findOne({email})
+    
+    const oldUser = await UserModel.findOne({email})
     // If user exist
-    if(user){
+    if(oldUser){
         return response.json({
             message: "Email already exist",
             error: true,
             success: false
         })
-        
     }
+
+    const passwordHash = await bcrypt.hash(password, 13)
+
+    // creating instance of user model
+    const user = new UserModel({
+      name,
+      email,
+      password: passwordHash
+    })
+
+    // Save user to DB
+    const savedUser = await user.save()
+    
+
+    response.json({
+      message: "User created successfully", 
+      data: savedUser
+    })
+
   } catch (err) {
     return response.status(500).json({
       message: err.message || err,
--- server/utils/validation.js
+++ server/utils/validation.js
@@ -1,7 +1,15 @@
-const validateSignUpData =(req)=>{
+import validator from 'validator'
+
+export const validateSignUpData =(req)=>{
     const { name, email, password } = req.body
 
     if(!name){
         throw new Error("First name and last name are required");
-    }else if(!validato)
+    }else if(!validator.isemail(email)){
+        throw new Error("Email is not valid");
+    }else if(!validator.isStrongPassword(password)){
+        throw new Error("Password is not strong enough");
+    }
 }
+
+
