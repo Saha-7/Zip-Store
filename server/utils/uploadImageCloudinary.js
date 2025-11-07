0641705054a882ab836f407aadd6c6a138f3fdd7
saha-7op <saha7pritam@gmail.com>
2025-11-07 21:24:15 +0530

    Cloudinary secrets setup

--- server/utils/uploadImageCloudinary.js
+++ server/utils/uploadImageCloudinary.js
@@ -1,8 +1,16 @@
 import { v2 as cloudinary } from 'cloudinary'
 
 
+// Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+
 const uploadImageCloudinary = async(image)=>{
-    const buffer = Buffer.from(await image.arrayBuffer())
+    const buffer = image?.buffer || Buffer.from(await image.arrayBuffer())
 
     const uploadImage = await new Promise((reslove, reject)=>{
         cloudinary.uploader.upload_stream({folder: "Zip Store"}, (error, uploadResult)=>{
