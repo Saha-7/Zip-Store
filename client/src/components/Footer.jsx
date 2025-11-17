331e3b2045c783d1456f7eec61e0b71e431adc35
saha-7op <saha7pritam@gmail.com>
2025-11-17 22:45:29 +0530

    header footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,18 +3,26 @@ import { FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 
-
 const Footer = () => {
   return (
-    <footer className="border-t-gray-400 h-10 text-black">
-      <div className="container mx-auto p-4 text-center flex flex-col gap-2">
+    <footer className="border-t-gray-400  text-black">
+      <div className="container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between">
         <p>© Zip-Store copyright reserved [Pritam Saha] [2025]</p>
-      </div>
+      
       <div className="flex items-center justify-center gap-4 text-lg cursor-pointer">
-        <a><FaInstagram /></a>
-        <a><FaFacebook /></a>
-        <a><FaLinkedin/></a>
-        <a><FaXTwitter/></a>
+        <a href="" className="hover:text-primary-100">
+          <FaInstagram />
+        </a>
+        <a className="hover:text-primary-100">
+          <FaFacebook />
+        </a>
+        <a className="hover:text-primary-100">
+          <FaLinkedin />
+        </a>
+        <a className="hover:text-primary-100">
+          <FaXTwitter />
+        </a>
+      </div>
       </div>
     </footer>
   );
