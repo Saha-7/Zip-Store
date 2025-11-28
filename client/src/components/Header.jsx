04fda5e44cdf0ac3a013ae113824d26d15f6fb69
saha-7op <saha7pritam@gmail.com>
2025-11-28 12:58:18 +0530

    Header skeleton

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,29 @@
+import logo from "../assets/logo.png";
+import FullLogo from "../assets/FullLogo.png";
 
 const Header = () => {
   return (
-    <header className='h-17 shadow-lg sticky top-0'>
-      <div></div>
-      <div></div>
-    </header>
-  )
-}
+    <header className="h-20 shadow-lg sticky top-0">
+      <div className="container flex mx-auto items-center h-full px-1 justify-between">
+        {/* Logo */}
+        <div className="h-full ">
+          <div className="h-full flex justify-center items-center">
+            <img src={logo} alt="logo" className="rounded-lg hidden lg:block" width={80} height={60} />
+            <img src={logo} alt="logo" className="rounded-lg lg:hidden" width={60} height={30} />
+          </div>
+        </div>
+        {/* search */}
+          <div>
+            Search
+          </div>
 
-export default Header
+          {/* Login & Cart */}
+          <div>
+            Login & Cart
+          </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
