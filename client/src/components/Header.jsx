24d6341e6c499cd40a4375f132b18d3e0549e394
Pritam Saha <saha7pritam@gmail.com>
2025-11-30 20:16:33 +0530

    Testing (#17)
    * Otp Verify
    
    * reset password controller + verify otp route
    
    * Otp verify success
    
    * refreshToken route + controller
    
    * verify refreshToken
    
    * refresh token API
    
    * API docs list
    
    * changes in console
    
    * Client + TailwindCSS setup
    
    * routes + components
    
    * routing issue fixed
    
    * Footer section
    
    * header footer
    
    * assets added
    
    * Header skeleton
    
    * Search UI + Type Animation
    
    * search path
    
    * Search UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,31 @@
+import logo from "../assets/logo.png";
+import FullLogo from "../assets/FullLogo.png";
+import Search from "./Search";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className='h-17 shadow-lg'>
-      Header
-    </header>
-  )
-}
+    <header className="h-18 shadow-lg sticky top-0">
+      <div className="container flex mx-auto items-center h-full px-1 justify-between">
+        {/* Logo */}
+        <div className="h-full ">
+          <Link to={"/"} className="h-full flex justify-center items-center">
+            <img src={logo} alt="logo" className="rounded-lg hidden lg:block" width={70} height={60} />
+            <img src={logo} alt="logo" className="rounded-lg lg:hidden" width={60} height={30} />
+          </Link>
+        </div>
+        {/* search */}
+          <div>
+            <Search />
+          </div>
 
-export default Header
+          {/* Login & Cart */}
+          <div>
+            Login & Cart
+          </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ client/src/components/Search.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
+import { TypeAnimation } from 'react-type-animation';
+
+const Search = () => {
+  const navigate=useNavigate()
+  const location = useLocation()
+  console.log(location)
+
+  const [searchPage,setSearchPage] = useState(false)
+
+  useEffect(()=>{
+    const isSearch = location.pathname==="/search"
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setSearchPage(isSearch)
+  },[location])
+
+  console.log(searchPage)
+
+
+  const redirectedToSearch = () =>{
+    navigate("/search")
+  }
+
+  return (
+    <div  className="w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200">
+      <button className="flex justify-center h-full items-center p-2 group-focus-within:text-primary-200">
+        <FaSearch />
+      </button>
+
+      <div className="w-full h-full flex justify-center items-center">
+        {!searchPage? (
+          <div onClick={redirectedToSearch} className="w-full h-full pr-2 items-center flex justify-start">
+         <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Search "Milk"',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Search "Egg"',
+        1000,
+        'Search "Paneer"',
+        1000,
+        'Search "Rice"',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    />
+      </div>
+        ):(
+          <div className="w-full h-full">
+            <input className="w-full h-full bg-transparent outline-none"
+            autoFocus
+            type="text"
+            placeholder="Search for items"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Search
